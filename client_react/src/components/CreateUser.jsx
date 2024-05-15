037380192811178c3fde3d037d3b443c6938fee3
Nikex5 <nik.osipenko75@yandex.ru>
2024-05-15 16:02:56 +0300

    commit

--- client_react/src/components/CreateUser.jsx
+++ client_react/src/components/CreateUser.jsx
@@ -1,99 +1,103 @@
 import React from 'react';
 import { Button, Checkbox, Form, Input, InputNumber } from 'antd';
-const onFinish = (values) => {
-    console.log('Success:', values);
-    fetch(`/add_user?name=${values.username}&lastname=${values.lastname}&age=${values.age}&tg_login=${values.tglogin}`)
-        .then(res => res.json)
-        .then(res => {
-            if (res.response.length === 0) {
-                props.refresh()
-            }
-        })
-};
-const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-};
-const CreateUser = (props) => (
-    <Form
-        name="basic"
-        labelCol={{
-            span: 8,
-        }}
-        wrapperCol={{
-            span: 16,
-        }}
-        style={{
-            maxWidth: 600,
-        }}
-        initialValues={{
-            remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-    >
-        <Form.Item
-            label="Username"
-            name="username"
-            rules={[
-                {
-                    required: true,
-                    message: 'Please input your username!',
-                },
-            ]}
-        >
-            <Input />
-        </Form.Item>
 
-        <Form.Item
-            label="Lastname"
-            name="lastname"
-            rules={[
-                {
-                    required: true,
-                    message: 'Please input your lastname!',
-                },
-            ]}
-        >
-            <Input />
-        </Form.Item>
+const CreateUser = (props) => {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+        fetch(`/add_user?name=${values.username}&lastname=${values.lastname}&age=${values.age}&tg_login=${values.tglogin}`)
+            .then(res => res.json)
+            .then(res => {
+                if (res.response.length === 0) {
+                    props.refresh()
+                }
+            })
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+    return (
 
-        <Form.Item
-            label="Age"
-            name="age"
-            rules={[
-                {
-                    required: true,
-                    message: 'Please input your age!',
-                },
-            ]}
-        >
-            <InputNumber style={{ width: "100%" }} />
-        </Form.Item>
-
-        <Form.Item
-            label="Tg Login"
-            name="tglogin"
-            rules={[
-                {
-                    required: true,
-                    message: 'Please input your tg login!',
-                },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
+        <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
             wrapperCol={{
-                offset: 8,
                 span: 16,
             }}
+            style={{
+                maxWidth: 600,
+            }}
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
         >
-            <Button type="primary" htmlType="submit">
-                Создать
-            </Button>
-        </Form.Item>
-    </Form>
-);
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your username!',
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Lastname"
+                name="lastname"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your lastname!',
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Age"
+                name="age"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your age!',
+                    },
+                ]}
+            >
+                <InputNumber style={{ width: "100%" }} />
+            </Form.Item>
+
+            <Form.Item
+                label="Tg Login"
+                name="tglogin"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your tg login!',
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                }}
+            >
+                <Button type="primary" htmlType="submit">
+                    Создать
+                </Button>
+            </Form.Item>
+        </Form>
+    )
+};
 export default CreateUser;
